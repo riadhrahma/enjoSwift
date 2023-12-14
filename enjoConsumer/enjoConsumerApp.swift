ca2f100ce817b3bee3086371cd2b491093133161
riadhGharbiTFG <riadh@tixforgigs.com>
2023-12-14 10:55:14 +0100

    Initial Commit

---
+++ enjoConsumer/enjoConsumerApp.swift
@@ -0,0 +1,17 @@
+//
+//  enjoConsumerApp.swift
+//  enjoConsumer
+//
+//  Created by Riadh Gharbi on 14/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct enjoConsumerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
